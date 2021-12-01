02c703b6917ed2ea980b95b4ba1f616d1dad460c
Agustin Fernandez <aaferna@outlook.com.ar>
2021-12-01 10:56:23 -0300

    0.1

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+
+
+    
+<div class="header bg-gradient-primary ">
+  <div class="container">
+    <div class="header-body text-center mb-7">
+      <div class="row align-items-center py-4">
+        <div class="col-lg-12 text-right">
+          <a href="#" class="btn btn-sm btn-neutral">Nueva DB</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container mt--8 pb-5 py-8">
+  <div class="row">
+    <div class="col-md-12"> 
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Equipos Registrados</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+
+</script>
+<style scoped>
+.card {
+    position: relative;
+    display: flex;
+    flex-direction: inherit;
+    min-width: 0;
+    word-wrap: break-word;
+    border: 1px
+    solid rgba(0, 0, 0, .05);
+    border-radius: 0.375rem;
+    background-color: #fff;
+    background-clip: border-box;
+}
+</style>
